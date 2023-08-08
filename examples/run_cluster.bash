2d4cc99155b5581e5b4baa5e1acd82021eab69f7
ChrisSun99 <mengtisun@berkeley.edu>
2023-08-08 19:36:13 -0400

    fix cluster error

--- examples/run_cluster.bash
+++ examples/run_cluster.bash
@@ -10,7 +10,7 @@ dataset_sizes=(4 8 16 32 64 128 256 512)
 
 for size in "${dataset_sizes[@]}"; do
     for run_idx in {1..9}; do
-        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args=\"-screen 0 800x600x24\" python examples/contactnets_simple.py --structured --system=cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --dataset-size $size 'cube_$size' 'cube_${size}-${run_idx}'"
+        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args=\"-screen 0 800x600x24\" python3 examples/contactnets_simple.py --structured --system=cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --dataset-size $size 'cube_$size' 'cube_${size}-${run_idx}'"
         echo "Running: $cmd"
         eval $cmd
     done
