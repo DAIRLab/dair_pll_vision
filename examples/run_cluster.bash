1793772c44c30b1ad82bb7c62edd1a30c6723dc5
ChrisSun99 <mengtisun@berkeley.edu>
2023-08-08 14:16:17 -0400

    change bash file

--- examples/run_cluster.bash
+++ examples/run_cluster.bash
@@ -6,17 +6,12 @@
 #SBATCH --time=12:00:00
 #SBATCH --job-name='pll'
 
-# The list of dataset sizes you want to run the command for
 dataset_sizes=(4 8 16 32 64 128 256 512)
 
-# Loop through each dataset size
 for size in "${dataset_sizes[@]}"; do
-    # Loop for the run index
     for run_idx in {1..9}; do
-        # Construct the command with the current dataset size and name
-        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args="-screen 0 800x600x24" python examples/contactnets_simple.py --structured --system=cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --dataset-size $size 'polygon_$size' 'polygon_${size}-${run_idx}'"
+        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args="-screen 0 800x600x24" python examples/contactnets_simple.py --structured --system=cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --dataset-size $size 'cube_$size' 'cube_${size}-${run_idx}'"
         echo "Running: $cmd"
-        # Execute the command
-        eval "$cmd"
+        $cmd
     done
 done
