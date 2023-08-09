02010d1b9cd2ce33ecaabdbe9c86c70c86dc5c80
ChrisSun99 <mengtisun@berkeley.edu>
2023-08-09 20:15:04 +0000

    specify port

--- examples/run_cluster2.bash
+++ examples/run_cluster2.bash
@@ -2,7 +2,7 @@
 #SBATCH --gpus=0
 #SBATCH --mem-per-cpu=10G
 #SBATCH --cpus-per-task=6
-#SBATCH --cpus-per-gpu=4
+##SBATCH --cpus-per-gpu=4
 #SBATCH --qos=mp-med
 #SBATCH --time=12:00:00
 #SBATCH --job-name='pll_2'
@@ -14,7 +14,7 @@ dataset_sizes=(256 512)
 
 for size in "${dataset_sizes[@]}"; do
     for run_idx in {1..9}; do
-        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args=\"-screen 0 800x600x24\" python3 examples/contactnets_simple.py --structured --system=cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --loss-variation=1 --dataset-size $size 'gt_cube_$size' 'gt_cube_${size}-${run_idx}'"
+        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-num=10 --server-args=\"-screen 0 800x600x24\" python3 examples/contactnets_simple.py --structured --system=cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --loss-variation=1 --dataset-size $size 'gt_cube_$size' 'gt_cube_${size}-${run_idx}'"
         echo "Running: $cmd"
         eval $cmd
     done
