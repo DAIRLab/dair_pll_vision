6ca25198a0ca50fe9f1bc34cd7ca9f5e54a7f575
ChrisSun99 <mengtisun@berkeley.edu>
2023-08-09 14:38:56 -0400

    change cluster job setting

--- examples/run_cluster2.bash
+++ examples/run_cluster2.bash
@@ -2,7 +2,8 @@
 #SBATCH --gpus=0
 #SBATCH --mem-per-cpu=10G
 #SBATCH --cpus-per-gpu=4
-#SBATCH --qos=mp-med
+#SBATCH --qos=posa-high
+#SBATCH --partition=posa-compute
 #SBATCH --time=12:00:00
 #SBATCH --job-name='pll_2'
 
