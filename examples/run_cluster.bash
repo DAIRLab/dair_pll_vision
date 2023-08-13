4d94992e99687167c130fe8ad91bbed39e255f09
ChrisSun99 <mengtisun@berkeley.edu>
2023-08-13 17:27:41 -0400

    prepare for running custom cube dataset

--- examples/run_cluster.bash
+++ examples/run_cluster.bash
@@ -9,11 +9,11 @@
 source /home/mengti/workspace/dair_pll/pll_env/bin/activate;
 export PYTHONPATH=${PWD}:${PYTHONPATH}
 
-dataset_sizes=(64 128)
+dataset_sizes=(10)
 
 for size in "${dataset_sizes[@]}"; do
     for run_idx in {1..9}; do
-        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args=\"-screen 0 800x600x24\" python3 examples/contactnets_simple.py --structured --system=cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --loss-variation=1 --dataset-size $size 'gt_cube_$size' 'gt_cube_${size}-${run_idx}'"
+        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args=\"-screen 0 800x600x24\" python3 examples/bundlesdf_simple.py --structured --system=cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --loss-variation=1 --dataset-size $size 'cube_$size' 'cube_${size}-${run_idx}'"
         echo "Running: $cmd"
         eval $cmd
     done
