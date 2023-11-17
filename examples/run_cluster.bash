33da22652234d16032a1b51d75c28c1be9e58c4b
ChrisSun99 <mengtisun@berkeley.edu>
2023-11-17 19:59:27 +0000

    add cluster bash

--- examples/run_cluster.bash
+++ examples/run_cluster.bash
@@ -1,20 +1,14 @@
 #!/bin/bash
 #SBATCH --gpus=0
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=16G
 #SBATCH --cpus-per-task=8
 #SBATCH --qos=mp-med
 #SBATCH --time=12:00:00
 #SBATCH --job-name='pll'
 
-source /home/mengti/workspace/dair_pll/pll_env/bin/activate;
+source /home/mengti/workspace/dair_pll/venv/bin/activate;
 export PYTHONPATH=${PWD}:${PYTHONPATH}
 
-dataset_sizes=(9)
-
-for size in "${dataset_sizes[@]}"; do
-    for run_idx in {10..15}; do
-        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args=\"-screen 0 800x600x24\" python3 examples/bundlesdf_simple.py --structured --system=bundlesdf_cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --loss-variation=1 --inertia-params=0 --dataset-size $size 'final_gt_mesh' 'final_gt_mesh-${run_idx}'"
-        echo "Running: $cmd"
-        eval $cmd
-    done
-done
+cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args=\"-screen 0 800x600x24\" python3 examples/bundlesdf_simple.py --structured --system=cube --geometry=mesh --source=real --contactnets --regenerate --no-residual --loss-variation=1 --inertia-params=0 --dataset-size 10 'test_export' 'test_export-1'"
+echo "Running: $cmd"
+eval $cmd
---
+++ examples/run_cluster_exp.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --gpus=0
+#SBATCH --mem-per-cpu=10G
+#SBATCH --cpus-per-task=8
+#SBATCH --qos=mp-med
+#SBATCH --time=12:00:00
+#SBATCH --job-name='pll'
+
+source /home/mengti/workspace/dair_pll/pll_env/bin/activate;
+export PYTHONPATH=${PWD}:${PYTHONPATH}
+
+dataset_sizes=(9)
+
+for size in "${dataset_sizes[@]}"; do
+    for run_idx in {10..15}; do
+        cmd="WANDB__SERVICE_WAIT=300 PYTHONUNBUFFERED=1 xvfb-run --server-args=\"-screen 0 800x600x24\" python3 examples/bundlesdf_simple.py --structured --system=bundlesdf_cube --geometry=polygon --source=real --contactnets --regenerate --no-residual --loss-variation=1 --inertia-params=0 --dataset-size $size 'final_gt_mesh' 'final_gt_mesh-${run_idx}'"
+        echo "Running: $cmd"
+        eval $cmd
+    done
+done
