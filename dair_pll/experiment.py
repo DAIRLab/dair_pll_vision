2d9ada5faf7ff9d3275908a8a00fe7a291e6163f
ChrisSun99 <mengtisun@berkeley.edu>
2023-12-21 17:41:37 +0000

    remove print statement of inertia

--- dair_pll/experiment.py
+++ dair_pll/experiment.py
@@ -373,9 +373,6 @@ class SupervisedLearningExperiment(ABC):
             learned_system: System being trained.
             statistics: Summary statistics for learning process.
         """
-
-        print(f'{epoch=}, {learned_system.multibody_terms.lagrangian_terms.inertial_parameters=}')
-
         # begin recording wall-clock logging time.
         assert self.tensorboard_manager is not None
         start_log_time = time.time()
