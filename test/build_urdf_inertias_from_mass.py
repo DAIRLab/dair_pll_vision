26e97e6b55d05c069c99f06795ed050e8cb1a31c
Bibit Bianchini <ebianchini.mit@gmail.com>
2024-08-04 21:18:43 -0400

    Test script to help confirm drake_pytorch generated functions

--- test/build_urdf_inertias_from_mass.py
+++ test/build_urdf_inertias_from_mass.py
@@ -1,6 +1,8 @@
 """Inertia computations for Franka end effector, given each link's mass and
 geometric properties.  Assumes each link's mass is evenly distributed."""
 
+import pdb
+
 base = 'base'
 link = 'link'
 tip = 'tip'
@@ -28,3 +30,19 @@ compute_cylinder_inertias(masses[link], radii[link], heights[link])
 
 print(f'Tip:')
 compute_sphere_inertias(masses[tip], radii[tip])
+
+
+robot_panda_link1_m = 2.74
+robot_panda_link1_com = [0, -0.0324958, -0.0675818]
+robot_panda_link1_Ixx = 0.0180416958283
+robot_panda_link1_Ixy=0.0
+robot_panda_link1_Ixz=0.0
+robot_panda_link1_Iyy=0.0159136071891
+robot_panda_link1_Iyz=0.0046758424612
+robot_panda_link1_Izz=0.0062069082712
+
+# (robot_panda_link1_m * ((robot_panda_link1_I(2) / robot_panda_link1_m) + pow(robot_panda_link1_com(0), 2) + pow(robot_panda_link1_com(1), 2)))
+val = robot_panda_link1_m * (
+    (robot_panda_link1_Izz / robot_panda_link1_m) + \
+    (robot_panda_link1_com[0]**2) + (robot_panda_link1_com[1]**2))
+pdb.set_trace()
